eeef1df7a23b158f9ebaf7a4b467f9b78cbebcf6
Taher Barakat <taher.barakat12223@gmail.com>
2023-11-30 12:26:05 +0300

    fix hour hand logic

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ function getTime() {
   let date = new Date();
   let sec = date.getSeconds() * 6;
   let min = date.getMinutes() * 6;
-  let hour = date.getHours() * 7.5;
+  let hour = date.getHours() * 30;
 
-  document.querySelector(".sec").style.transform = `rotate(${sec}deg)`;
+  document.querySelector(".sec").style.transform = `rotate(${sec + 90}deg)`;
   document.querySelector(".min").style.transform = `rotate( ${min + 90}deg) `;
-  document.querySelector(".hour").style.transform = `rotate(${hour}deg)`;
+  document.querySelector(".hour").style.transform = `rotate(${hour + 90}deg)`;
 
   console.log(date.getSeconds(), date.getMinutes(), date.getHours());
 }
