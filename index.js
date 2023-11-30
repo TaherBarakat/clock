0859b71a5710e9968c947d7f510c082597f58828
Taher Barakat <taher.barakat12223@gmail.com>
2023-11-30 12:18:59 +0300

    add functionality

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-// function getTime() {
-//   let date = new Date();
-//   let sec = document.querySelector(".sec");
-//   let min = document.querySelector(".min");
-//   let hour = document.querySelector(".hour");
-// }
+function getTime() {
+  let date = new Date();
+  let sec = date.getSeconds() * 6;
+  let min = date.getMinutes() * 6;
+  let hour = date.getHours() * 7.5;
 
-// setInterval(function () {
-//   console.log(getTime().getSeconds());
-// }, 1000);
+  document.querySelector(".sec").style.transform = `rotate(${sec}deg)`;
+  document.querySelector(".min").style.transform = `rotate( ${min + 90}deg) `;
+  document.querySelector(".hour").style.transform = `rotate(${hour}deg)`;
+
+  console.log(date.getSeconds(), date.getMinutes(), date.getHours());
+}
+
+setInterval(function () {
+  getTime();
+}, 1000);
