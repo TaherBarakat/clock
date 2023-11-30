6a1c22c8848575ee32a4d495bfe2ce29300262fa
Taher Barakat <taher.barakat12223@gmail.com>
2023-11-30 10:44:08 +0300

    init

---
+++ index.js
@@ -0,0 +1,10 @@
+// function getTime() {
+//   let date = new Date();
+//   let sec = document.querySelector(".sec");
+//   let min = document.querySelector(".min");
+//   let hour = document.querySelector(".hour");
+// }
+
+// setInterval(function () {
+//   console.log(getTime().getSeconds());
+// }, 1000);
